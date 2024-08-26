3a831c5bf121bf4fb0618f09deda8003ccf39b0c
Najim Uddin <engr.najim3@gmail.com>
2024-08-26 10:49:37 +0600

    2. searchParams

--- intro/src/app/posts/page.js
+++ intro/src/app/posts/page.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Post = () => {
+const Post = ({ searchParams }) => {
+  console.log("searchParams from server component:", searchParams);
+  console.log("searchParams from server component:", searchParams.search);
+
   return <div>Post</div>;
 };
 

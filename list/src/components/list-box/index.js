1d2ab0c615b4f9937451dac50fdde1869e52fd9d
Najim Uddin <engr.najim3@gmail.com>
2024-08-31 16:19:38 +0600

    # POST API route

--- list/src/components/list-box/index.js
+++ list/src/components/list-box/index.js
@@ -1,22 +1,42 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 const ListBox = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
   return (
     <div>
-      <form className="flex flex-col">
+      <form
+        className="flex flex-col"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("formData: ", formData);
+        }}
+      >
         <label htmlFor="title">Title</label>
         <input
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+          value={formData.title}
           type="text"
           name="title"
           className="text-sm rounded-lg block focus:ring-blue-500  w-full p-2.5 border-gray-600 placeholder-gray-400 text-white bg-gray-700"
         />
         <label htmlFor="description">Description</label>
         <input
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+          value={formData.description}
           type="text"
           name="description"
           className="h-24 text-sm rounded-lg block focus:ring-blue-500  w-full p-2.5 border-gray-600 placeholder-gray-400 text-white bg-gray-700"
         />
-        <button className="px-6 my-4 text-sm rounded-lg block focus:ring-blue-500  w-full p-2.5 border-gray-600 placeholder-gray-400 text-white bg-gray-900">
+        <button
+          type="submit"
+          className="px-6 my-4 text-sm rounded-lg block focus:ring-blue-500  w-full p-2.5 border-gray-600 placeholder-gray-400 text-white bg-gray-900"
+        >
           Create
         </button>
       </form>
